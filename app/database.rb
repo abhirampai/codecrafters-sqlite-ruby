77e1ba867379cf94c5d922e723a41040486b2c68
Abhiram Pai <abhirampai1999@gmail.com>
2024-09-24 21:11:09 +0530

    Misc refactoring

--- app/database.rb
+++ app/database.rb
@@ -1,21 +1,43 @@
 # frozen_string_literal: true
 
+require_relative 'database/sqlite_schema'
+
 # Main database class
 # returns the database file instance
 class Database
-  attr_reader :file
+  attr_reader :file, :page_size, :table_size, :table_names
 
   def initialize(database_file_path)
     @file = File.open(database_file_path, 'rb')
+    load_database_header_details
   end
 
-  def page_size
+  def load_database_header_details
+    @page_size = load_page_size
+    @table_size = load_table_size
+    @table_names = load_table_names
+  end
+
+  private
+
+  def load_page_size
     file.seek(16)
     file.read(2).unpack1('n')
   end
 
-  def table_size
+  def load_table_size
     file.seek(103)
     file.read(2).unpack1('n')
+  end
+
+  def load_table_names
+    file.seek(108)
+    column_pointers = table_size.times.map { |_| file.read(2).unpack1('n') + 1 }
+    sqlite_schema_rows = column_pointers.map do |pointers|
+      file.seek(pointers)
+      SqliteSchema.new(file)
+    end
+
+    sqlite_schema_rows.map(&:tbl_name).join(' ')
   end
 end
--- app/main.rb
+++ app/main.rb
@@ -1,30 +1,15 @@
 # frozen_string_literal: true
 
 require_relative 'database'
-require_relative 'database/sqlite_schema'
 
 database_file_path = ARGV[0]
 command = ARGV[1]
 
 database = Database.new(database_file_path)
-database_file = database.file
-page_size = database.page_size
-table_size = database.table_size
 
 if command == '.dbinfo'
-  puts "database page size: #{page_size}"
-  puts "number of tables: #{table_size}"
-end
-
-if command == '.tables'
-  database_file.seek(108)
-
-  column_pointers = table_size.times.map { |_| database_file.read(2).unpack1('n') + 1 }
-
-  sqlite_schema_rows = column_pointers.map do |pointers|
-    database_file.seek(pointers)
-    SqliteSchema.new(database_file)
-  end
-
-  puts sqlite_schema_rows.map(&:tbl_name).join(' ')
+  puts "database page size: #{database.page_size}"
+  puts "number of tables: #{database.table_size}"
+elsif command == '.tables'
+  puts database.table_names
 end
